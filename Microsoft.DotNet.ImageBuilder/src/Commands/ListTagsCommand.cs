77dbab54b84e50037a3f7887a452424acf8d5d1d
MichaelSimons <msimons@microsoft.com>
2019-01-29 14:05:58 -0600

    edits

--- Microsoft.DotNet.ImageBuilder/src/Commands/ListTagsCommand.cs
+++ Microsoft.DotNet.ImageBuilder/src/Commands/ListTagsCommand.cs
@@ -18,26 +18,56 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
         public override Task ExecuteAsync()
         {
             Logger.WriteHeading("GENERATING TAGS LIST");
-            foreach (RepoInfo repo in Manifest.Repos)
+            foreach (RepoInfo repo in Manifest.FilteredRepos)
             {
-                var variantGroups = repo.Images
-                    .Select(image => new {
-                            Variant = image.Platforms.First().DockerfilePath.Split('/')[1],
-                            Tags = image.SharedTags.Concat(image.Platforms.SelectMany(platform => platform.Tags))
-                        })
+                var variantGroups = repo.AllImages
+                    .Select(image => new
+                    {
+                        Variant = image.AllPlatforms.First().DockerfilePath.Split('/')[1],
+                        Tags = image.SharedTags.Concat(image.AllPlatforms.SelectMany(platform => platform.Tags))
+                    })
                     .GroupBy(info => info.Variant);
 
                 foreach (var variantGroup in variantGroups)
                 {
                     Logger.WriteSubheading($"{variantGroup.Key}:");
                     string tags = variantGroup.SelectMany(info => info.Tags)
-                        .Select(tag => $"    - source: {tag.Name}{Environment.NewLine}      target: {tag.Name}")
+                        .Select(tag => GetTagYaml(tag))
                         .Aggregate((working, next) => $"{working}{Environment.NewLine}{next}");
                     Logger.WriteMessage(tags);
                 }
             }
 
             return Task.CompletedTask;
+        }
+
+        private string GetTagYaml(TagInfo tag)
+        {
+            string sourceTag = tag.Model.IsUndocumented ? "<UNDOCUMENTED>" : tag.Name;
+            string[] variants = new string [] { "runtime-deps", "aspnetcore-runtime", "runtime", "sdk" };
+            foreach (string variant in variants)
+            {
+                int index = sourceTag.IndexOf(variant);
+                if (index != -1)
+                {
+                    sourceTag = sourceTag.Remove(index, variant.Length);
+                }
+
+                index = sourceTag.IndexOf("--");
+                if (index != -1)
+                {
+                    sourceTag = sourceTag.Remove(index, 1);
+                }
+
+                sourceTag = sourceTag.TrimEnd('-');
+            }
+
+            if (sourceTag == string.Empty)
+            {
+                sourceTag = "<TODO>";
+            }
+
+            return $"    - source: {sourceTag}{Environment.NewLine}      target: {tag.Name}";
         }
     }
 }
